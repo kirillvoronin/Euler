614f0c8a7b730c1248fcdcf6e77fafbd09150dff
YuIsAwsm <im@borntodie.ru>
2017-02-14 22:30:10 +0300

    updated the 3rd problem

--- 003.py
+++ 003.py
@@ -1,19 +1,18 @@
-import math
+from math import sqrt
 
+# used brute force method
 
 def main():
     n = 600851475143
-    #n = 13195
     maximum = 0
-    for x in range(2,int(math.sqrt(n))):
+    for x in range(2,int(sqrt(n))):
         if n % x == 0:
-            divs = 0
+            isPrime = True
             for y in range(2,x-1):
                 if x % y == 0:
-                    divs += 1
-            if divs == 0:
-                if x > maximum:
-                    maximum = x
+                    isPrime = False
+            if isPrime:
+                maximum = x
 
     return maximum
 
