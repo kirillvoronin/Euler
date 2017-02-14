fa4c2770bacf2df992120c2ddea1a54a28ad5d12
YuIsAwsm <im@borntodie.ru>
2017-02-14 21:17:46 +0300

    1 and 2

---
+++ 002.py
@@ -0,0 +1,21 @@
+
+
+def main():
+    x = 1
+    y = 1
+    summ = 0
+    result = 0
+    while result < 4000000:
+        if x % 2 == 0:
+            summ += x
+
+        #x, y = y, x + y
+        result = x + y
+        y = x
+        x = result
+
+    return summ
+
+
+if __name__ == '__main__':
+    print(main())
