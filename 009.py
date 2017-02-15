7379b89f6209169856176c153e091ce6308c13b8
YuIsAwsm <im@borntodie.ru>
2017-02-15 22:48:37 +0300

    upd prob 9

--- 009.py
+++ 009.py
@@ -4,11 +4,12 @@ def main():
 
 
 
-    for x in range(2,1000+1):
-        for y in range(x+1, 1000+1):
-            z = sqrt(x**2 + y**2)
-            if z.is_integer() and (x+y+z) == 1000:
-                return (x*y*z)
+    for x in range(2,1000 + 1):
+        for y in range(x + 1, 1000 + 1):
+            #z = sqrt(x ** 2 + y ** 2)
+            z = 1000 - (x+y)
+            if (x + y + z) == 1000 and (x ** 2 + y ** 2 == z ** 2):
+                return (x * y * z)
 
 
 
