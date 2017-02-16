ef8bb470c19fe649376e6088ec07cc184d4d6d36
YuIsAwsm <im@borntodie.ru>
2017-02-16 16:19:25 +0300

    Problem 12

--- 012.py
+++ 012.py
@@ -4,17 +4,18 @@ def main():
     x = 1
     triangle = [1]
     while True:
+
         triangle.append(x+1)
         x += 1
         res = sum(triangle[0:x])
         deli = 0
+        
         for y in range(1,int(sqrt(res))):
-            if res % y == 0:
-                deli += 2
-                print(x, res, deli)
-        if deli > 500:
-            break
+            if res % y == 0: deli += 2
 
+        if deli > 500: break
+
+    return res
 
 if __name__ == '__main__':
     print(main())
