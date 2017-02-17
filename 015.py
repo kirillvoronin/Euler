a393aa28813ee48966a58c62e05faddfe81b3708
YuIsAwsm <im@borntodie.ru>
2017-02-17 17:41:25 +0300

    Problem 15

---
+++ 015.py
@@ -0,0 +1,15 @@
+def main():
+
+    gridSize = 20
+    ways = 1
+
+    for x in range(gridSize):
+
+        ways = ways * ((2 * gridSize) - x)/(x+1)
+
+
+
+    return int(ways)
+
+if __name__ == '__main__':
+    print(main())
