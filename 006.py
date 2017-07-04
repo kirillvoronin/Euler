5d93464ac2f94bdbcdceb35ecca54ba1af091199
YuIsAwsm <im@borntodie.ru>
2017-07-04 21:11:36 +0300

    add: welcome javascript

--- 006.py
+++
@@ -1,5 +0,0 @@
-def main():
-    return sum(range(1,101))**2 - sum(x**2 for x in range(1,101))
-
-if __name__ == '__main__':
-    main()
--- 016.py
+++
@@ -1,7 +0,0 @@
-def main():
-
-    return sum([int(x) for x in list(str(2**1000))])
-
-
-if __name__ == '__main__':
-    print(main())
