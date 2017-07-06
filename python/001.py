c5b98e14a5be15a90842dcbbe7cc25d75586bd2d
Kirill V <im@borntodie.ru>
2017-07-07 02:24:45 +0300

    Update 001.py

--- python/001.py
+++ python/001.py
@@ -1,3 +1,18 @@
+"""
+
+Problem: Find the sum of all the multiples of 3 or 5 below 1000.
+
+We can solve this problem with brutforce. 
+Computers are powerful and fast, so stuff like that is easy to compute even with brutforce.
+
+int(x) = 999 / 3
+int(y) = 995 / 5
+int(z) = 990 / 15
+
+int(x) * math.ceil(int(x)/2) * 3 + int(y) * math.ceil(int(y)/2) * 5 - (int(z)+1) * math.ceil(int(z)/2) * 15
+
+"""
+
 
 def main():
     return sum(x for x in range(1000) if (x % 3 == 0 or x % 5 == 0))
