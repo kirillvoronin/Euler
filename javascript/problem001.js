f20eb18fceded81366371eadab16c39c92e24d36
Kirill V <im@borntodie.ru>
2017-07-08 22:32:27 +0300

    Update problem001.js

--- javascript/problem001.js
+++ javascript/problem001.js
@@ -1,3 +1,18 @@
+/*
+
+Problem: Find the sum of all the multiples of 3 or 5 below 1000.
+
+We can solve this problem with brutforce. 
+Computers are powerful and fast, so stuff like that is easy to compute even with brutforce.
+
+int(x) = 999 / 3
+int(y) = 995 / 5
+int(z) = 990 / 15
+
+int(x) * math.ceil(int(x)/2) * 3 + int(y) * math.ceil(int(y)/2) * 5 - (int(z)+1) * math.ceil(int(z)/2) * 15
+
+*/
+
 /* eslint no-console: ["error", { allow: ["log"] }] */
 function getResult() {
   let sum = 0;
