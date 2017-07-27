1111f54b6228dd0a67ab3c5416c781358420aed1
K <im@borntodie.ru>
2017-07-27 19:26:45 +0300

    Rename 055.py to 056.py

---
+++ python/056.py
@@ -0,0 +1,9 @@
+"""
+Q: Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
+
+"""
+
+def main(): 
+    return max(sum(int(x) for x in str(y ** z)) for y in range(100) for z in range(100))
+      
+print(main())
