22b0473a47a7a9058e56e5e61dd2e951088eef18
K <voronin@yahoo.com>
2017-07-29 05:35:49 +0300

    Update 056.py

--- python/056.py
+++ python/056.py
@@ -6,5 +6,6 @@ Q: Considering natural numbers of the form, ab, where a, b < 100, what is the ma
 
 def main(): 
     return max(sum(int(x) for x in str(y ** z)) for y in range(100) for z in range(100))
-      
-print(main())
+
+if __name__ == "__main__":
+    print(main())
