fa0472710f302bdfc7e95eaf59c97f491d556abc
k <voronin@yahoo.com>
2017-07-30 22:44:06 +0300

    Update 015.py

--- python/015.py
+++ python/015.py
@@ -2,12 +2,8 @@ def main():
 
     gridSize = 20
     ways = 1
-
     for x in range(gridSize):
-
-        ways = ways * ((2 * gridSize) - x)/(x+1)
-
-
+        ways = ways * ((2 * gridSize) - x)/(x + 1)
 
     return int(ways)
 
